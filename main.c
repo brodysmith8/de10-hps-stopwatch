9b40e4a51c6c51506bfa4b2046fde9ceee297c96
Brody Smith <bsmit272@uwo.ca>
2023-02-21 11:29:24 -0500

    Read control buttons

--- main.c
+++ main.c
@@ -22,13 +22,14 @@ Address     Switch  Bit Position    Function    Description
             KEY1    Bit 1           Stop        Stop the stopwatch
             KEY2    Bit 2           Lap         Store current time
             KEY3    Bit 3           Clear       Resets the stopwatch back to zero
-0xFF200040  SW0     Bit 0           Display     Toggle between displaying current time and lap time
+0xFF200040  SW0     Bit 0           Display     Toggle between displaying current time (0) and lap time (1)
 */
 
 #include "seven_segment_display.h"
 
-#define PRIVATE_TIMER_BASE 0xFFFEC600
-#define SW_BASE         0xFF200040
+#define PRIVATE_TIMER_BASE  0xFFFEC600
+#define SW_BASE             0xFF200040
+#define BUTTON_BASE         0xFF200050
 
 // SW0 is the bit at the base, SW1 is the next, etc
 volatile int* const switch_bank_ptr = (int *)(SW_BASE);
@@ -36,14 +37,35 @@ int read_switches(void) {
     return *switch_bank_ptr; 
 }
 
+volatile int* const button_bank_ptr = (int *)(BUTTON_BASE);
+void read_buttons_to_buffer(char* buffer) {
+    *buffer = *button_bank_ptr & 0b1000;
+    *(buffer + 0x1) = *button_bank_ptr & 0b0100;
+    *(buffer + 0x2) = *button_bank_ptr & 0b0010;
+    *(buffer + 0x3) = *button_bank_ptr & 0b0001;
+}
+
 volatile int DELAY_LENGTH = 700000;
 int main(void) {
     //display_hex(-345434);
     int num;
+    int display_mode = 0;
     volatile int delay_count = 0;
+
+    // bit 3, bit 2, bit 1, bit 0
+    // clear, lap,   stop,  start
+    char control_bits[4] = {0};
     while(1) {
         // on 
         num = read_switches();
+        if (num != 0) {
+            display_mode = 1;
+        } else {
+            display_mode = 0;
+        }
+
+        read_buttons_to_buffer(&control_bits[0]);
+
         display_hex(num);
 
         for (delay_count = DELAY_LENGTH; delay_count != 0; --delay_count) {}
@@ -53,4 +75,3 @@ int main(void) {
         for (delay_count = DELAY_LENGTH; delay_count != 0; --delay_count) {}
     }
 }
-
